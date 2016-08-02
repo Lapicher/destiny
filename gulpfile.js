91a6e2a89034e6622b8b8215f1df4f2b02bdbe1e
Nataly Contreras <nataly.contreras@cidesi.edu.mx>
2016-08-01 21:52:51 -0500

    se agrega optimizacion o minimizado de css y javascript

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,11 @@ var concat= require('gulp-concat');
 var browserify= require('browserify');
 var tap=require('gulp-tap');
 var buffer=require('gulp-buffer');
+var uglify=require('gulp-uglify');
+var sourcemaps=require('gulp-sourcemaps');
+var postcss=require('gulp-postcss');
+var autoprefixer=require('autoprefixer');
+var cssnano=require('cssnano');
 
 //variables de patrones de archivos
 var jsFiles=["src/js/*.js","src/js/**/*.js"];
@@ -34,7 +39,14 @@ gulp.task("default", ["concat-js","compile-sass"], function(){
 // definimos la tarea para compilar SASS
 gulp.task("compile-sass", function(){
     gulp.src("./src/scss/style.scss") // cargamos le archivo
+    .pipe(sourcemaps.init()) //comenzamos la captura de sourcemaps
     .pipe(sass().on('error', sass.logError)) // compilamos el archivo SASS
+
+    .pipe(postcss([
+      autoprefixer(), // autoprefija automaticamente el css
+      cssnano() //minifica el CSS.
+    ]))
+    .pipe(sourcemaps.write('./')) //escribimos los sourcemaps.
     .pipe(gulp.dest("./dist/css/")) // guardamos el archivo en dist/css
     .pipe(notify({
         title: "SASS",
@@ -46,10 +58,13 @@ gulp.task("compile-sass", function(){
 //definimos la tarea para concatenar JS
 gulp.task("concat-js",function(){
     gulp.src("src/js/app.js")
+    .pipe(sourcemaps.init()) // comenzamos la captura de sourcemaps.
     .pipe(tap(function(file){
         file.contents=browserify(file.path).bundle(); //pasamos el archivo por broserify para importar los require.
     }))// tap nos permite ejecutar un codigo por cada fichero seleccionado en el paso anterior
     .pipe(buffer())// converir cada archivo en un stream.
+    .pipe(uglify())
+    .pipe(sourcemaps.write('./')) // escribimos los sourcemaps.
     .pipe(gulp.dest("dist/js/"))
     .pipe(notify({
         title: "JS",
